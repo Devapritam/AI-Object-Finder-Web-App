92138066d77ad5c50a7eb30444a0faf4a1d35f50
Devapritam Sharma <dsarma1980@gmail.com>
2021-10-19 12:58:33 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
-console.log("Hello World!");
+status = "";
+// btn = document.getElementsByClassName("start-btn");
+
+function setup() {
+    canvas = createCanvas(480, 380);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(480, 380);
+    video.hide();
+}
+
+function start() {
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status - Detecting Objects";
+    inputValue = document.getElementById("objectName").value;
+}
+
+function draw() {
+    image(video, 0, 0, 480, 380);
+}
+
+function modelLoaded() {
+    window.alert("Cocossd model is successfully loaded in the web app");
+    status = true;
+}

8bcfae659c0ff81478be0a637d16babb0675fb76
Devapritam Sharma <dsarma1980@gmail.com>
2021-10-22 08:13:16 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+objects = [];
 status = "";
 // btn = document.getElementsByClassName("start-btn");
 
@@ -20,6 +21,45 @@ function modelLoaded() {
     status = true;
 }
 
+function gotResult(error, results) {
+    if (error) {
+        console.error(error);
+    }
+    else {
+        console.log(results);
+        objects = results;
+    }
+}
+
 function draw() {
     image(video, 0, 0, 480, 380);
+    if (status != "") {
+        objectDetector.detect(video, gotResult);
+
+        for (var i = 0; i < objects.length; i++) {
+            document.getElementById("status").innerHTML = "Status - Objects Detected";
+
+            fill('#F10000');
+            percent = objects[i].confidence * 100;
+            text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
+            noFill();
+            stroke('#F10000');
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+
+            if(inputValue == objects[i].label) {
+                video.stop();
+                objectDetector.detect(gotResult);
+                document.getElementById("objectStatus").innerHTML = "Object Mentioned Found";
+
+                var synth = window.speechSynthesis;
+                var data = inputValue + " found";
+                var utterThis = new SpeechSynthesisUtterance(data);
+                synth.speak(utterThis);
+            }
+
+            else {
+                document.getElementById("objectStatus").innerHTML = "Object Mentioned Not Found";
+            }
+        }
+    }
 }
